bdd13914d304449ba0fab7465972ab9e51d70014
Athan Clark <athan.clark@gmail.com>
2017-12-23 16:28:11 -0700

    exposure

--- src/System/Pty.purs
+++ src/System/Pty.purs
@@ -1,9 +1,11 @@
-module System.Pty where
+module System.Pty
+  ( PtyProcess, PTY, PtySpawnArgs, PtySpawnParams, spawn, write, onData, resize
+  ) where
 
 import Prelude
 import Data.StrMap (StrMap)
 import Control.Monad.Eff (Eff, kind Effect)
-import Control.Monad.Eff.Uncurried (EffFn1, EffFn2, EffFn3)
+import Control.Monad.Eff.Uncurried (EffFn1, EffFn2, EffFn3, mkEffFn1, runEffFn2, runEffFn3)
 
 
 
@@ -23,8 +25,20 @@ type PtySpawnParams =
 
 foreign import spawnImpl :: forall eff. EffFn3 (pty :: PTY | eff) String (Array PtySpawnArgs) PtySpawnParams PtyProcess
 
+spawn :: forall eff. String -> Array PtySpawnArgs -> PtySpawnParams -> Eff (pty :: PTY | eff) PtyProcess
+spawn = runEffFn3 spawnImpl
+
 foreign import writeImpl :: forall eff. EffFn2 (pty :: PTY | eff) PtyProcess String Unit
+
+write :: forall eff. PtyProcess -> String -> Eff (pty :: PTY | eff) Unit
+write = runEffFn2 writeImpl
 
 foreign import onDataImpl :: forall eff. EffFn2 (pty :: PTY | eff) PtyProcess (EffFn1 (pty :: PTY | eff) String Unit) Unit
 
+onData :: forall eff. PtyProcess -> (String -> Eff (pty :: PTY | eff) Unit) -> Eff (pty :: PTY | eff) Unit
+onData p f = runEffFn2 onDataImpl p (mkEffFn1 f)
+
 foreign import resizeImpl :: forall eff. EffFn2 (pty :: PTY | eff) PtyProcess {cols :: Int, rows :: Int} Unit
+
+resize :: forall eff. PtyProcess -> {cols :: Int, rows :: Int} -> Eff (pty :: PTY | eff) Unit
+resize = runEffFn2 resizeImpl
